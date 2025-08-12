010341faf6c7a9520cb9644946da625a4e8d9442
Enisco29 <opeyemioluwafisayo29@gmail.com>
2025-08-12 15:13:20 +0100

    add

--- screenshot.js
+++ screenshot.js
@@ -1,16 +1,11 @@
 import puppeteer from "puppeteer";
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
-
-  await page.goto("http://localhost:5173", {
-    waitUntil: "networkidle2",
-  });
-
-  await page.screenshot({
-    path: "homepage.png",
-    fullPage: true,
-  });
+  await page.goto("http://localhost:5173", { waitUntil: "networkidle2" });
+  await page.screenshot({ path: "homepage.png" });
   await browser.close();
 })();
